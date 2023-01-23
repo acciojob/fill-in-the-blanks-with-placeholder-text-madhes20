59cd4f3fdccde291a0b036ed8376947f2d03e012
madhes20 <118300068+madhes20@users.noreply.github.com>
2023-01-23 18:18:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 //your code here
+let para = document.getElementsByTagName("p");
+para[0].innerText="Accio ipsum dolor sit amet, join course, solve assignments, get placed above 5 lpa and live life king size."
